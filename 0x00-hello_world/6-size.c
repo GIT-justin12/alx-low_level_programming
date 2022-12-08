176e2f3200526b5d1ab906e53def4bdfe953cc27
GIT-justin12 <agbozojustin42@gmail.com>
2022-12-08 11:02:06 -0800

    Correct puts programs

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *  main - Entry
+ *  Return: Always 0
+ */
+int main(void)
+{
+	printf("Size of char: %i byte(s)\n", sizerof(char));
+	printf("Size of char: %i byte(s)\n", sizerof(char));
+	printf("Size of char: %i byte(s)\n", sizerof(char));
+	printf("Size of char: %i byte(s)\n", sizerof(char));
+	printf("Size of char: %i byte(s)\n", sizerof(char));
