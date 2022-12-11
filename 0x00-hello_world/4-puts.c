fd04529ee0cab3f2755f2d5c19203d8ff0cb5cbd
GIT-justin12 <agbozojustin42@gmail.com>
2022-12-11 12:28:14 -0800

    Correction of the task Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - Entry piont 
+ * main - Entry point
+ *
  * Return: Always 0 (sucess)
  */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
 }
