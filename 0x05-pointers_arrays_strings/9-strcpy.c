051611362a43fa717548b555d01efaf79686032a
GIT-justin12 <agbozojustin42@gmail.com>
2022-12-20 11:46:27 -0800

    task 3

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcpy -  copies the string pointed to by src
+ * @dest: This is destiny
+ * @src: This the copy
+ * Return: the pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	char *start = dest;
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+
+	return (start);
+}
+
