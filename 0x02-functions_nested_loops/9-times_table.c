74c35a4ca75638fdba1474f27eb59e61ea8e1be0
GIT-justin12 <agbozojustin42@gmail.com>
2022-12-24 11:57:19 -0800

    Correct 3

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,7 +12,7 @@ void times_table(void)
 	{
 		_putchar('0');
 
-		for (b = 0; b <= 9; b++)
+		for (b = 1; b <= 9; b++)
 		{
 			_putchar(',');
 			_putchar(' ');
@@ -20,7 +20,7 @@ void times_table(void)
 			c = a * b;
 
 			if (c <= 9)
-				_putchar(' ');
+				_putchar(c);
 			else
 				_putchar((c / 10) + '0');
 		}
