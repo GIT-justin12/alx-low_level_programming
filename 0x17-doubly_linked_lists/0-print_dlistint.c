b3167bace8650c4ff1537977829e2482e44d7674
GIT-justin12 <agbozojustin42@gmail.com>
2023-03-17 12:18:06 -0700

    Task 0: update 1

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -11,14 +11,14 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t numberOfNodes = 0;
+	size_t nodes = 0;
 
 	while (h)
 	{
-		numberOfNodes++;
+		nodes++;
 		printf("%d\n", h->n);
 		h = h->next;
 	}
 
-	return (numberOfNodes);
+	return (nodes);
 }
