e417e8c7f4d141ff57b2d2b273f0dec56c21e95a
GIT-justin12 <agbojustin42@gmail.com>
2023-09-08 10:45:32 -0700

    Task 0: It ain't what they call you, it's what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Print the program's name.
+ * @argc: Count the number of parameter.
+ * @argv: Vector of string
+ *
+ * Return: Always(0).
+ */
+int main(int argc, char *argv[])
+{
+(void) argc;
+printf("%s\n", argv[0]);
+
+return (0);
+}
