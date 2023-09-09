287ef0bf04dd1e4fd44f2a7a2bdb761b925be130
GIT-justin12 <agbojustin42@gmail.com>
2023-09-08 17:10:10 -0700

    Task 3: Neither irony nor sarcasm is argument

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - Multiplies two numbers.
+ * @argc: Count argument.
+ * @argv: Content argument.
+ *
+ * Return: Always(0).
+ */
+int main(int argc, char *argv[])
+{
+	int result;
+
+	if (argc == 3)
+	{
+		result = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d", result);
+	} else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
