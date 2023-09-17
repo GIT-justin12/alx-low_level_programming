f13b45c33e30b7fe7ea04124b357a0884ee5cc22
GIT-justin12 <agbojustin42@gmail.com>
2023-09-17 14:31:25 -0700

    Update 2 of task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,42 +12,43 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	char *s3;
-	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
+	int i = 0, j = 0, k = 0, l = 0;
+	char *s;
 
-	while (s1 && s1[len1])
-		len1++;
+	if (s1 == NULL)
+		s1 = "";
 
-	while (s2 && s2[len2])
-		len2++;
+	if (s2 == NULL)
+		s2 = "";
 
-	s3 = malloc(sizeof(char) * (len1 + len2 + 1));
+	while (s1[i])
+		i++;
 
-	if (s3 == NULL)
+	while (s2[i])
+		j++;
+
+	l = i + j;
+	s = malloc((sizeof(char) * l) + 1);
+
+	if (s == NULL)
 		return (NULL);
 
-	i = 0;
 	j = 0;
 
-	if (s1)
+	while (k < l)
 	{
-		while (i < len1)
-		{
-			s3[i] = s1[i];
-			i++;
-		}
-	}
+		if (k <= i)
+			s[k] = s1[k];
 
-	if (s2)
-	{
-		while (i < (len1 + len2))
+		if (k >= i)
 		{
-			s3[i] = s2[i];
-			i++;
+			s[k] = s2[j];
 			j++;
 		}
-	}
-		s3[i] = '\0';
 
-		return (s3);
+		k++;
+	}
+
+	s[k] = '\0';
+	return (s);
 }
