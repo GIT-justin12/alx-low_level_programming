b544fadfce071077120ff4f83a022f06c82983aa
GIT-justin12 <agbojustin42@gmail.com>
2023-09-24 07:07:32 -0700

    Task 2: Unconditional love of dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_dog - Print a struct dog.
+ * @d: A dog struct.
+ *
+ * Return: Nothing.
+ */
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		printf("Name: %s\n", (d->name) ? d->name : "(nil)");
+		printf("Age: %f\n", (d->age) ? d->age : "(nil)");
+		printf("Owner: %s\n", (d->owner) ? d->owner : "(nil)");
+	}
+}
