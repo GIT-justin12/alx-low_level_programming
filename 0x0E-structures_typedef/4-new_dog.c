8c310f736aeeb3f590d2dd3781ea4a79344e6bd1
GIT-justin12 <agbojustin42@gmail.com>
2023-09-24 13:41:30 -0700

    Update 1 of task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -53,6 +53,23 @@ dog_t *new_dog(char *name, float age, char *owner)
 }
 
 /**
+ * _strlen - Returns the length of a string.
+ * @s: String to count.
+ *
+ * Return: String length.
+ */
+int _strlen(char *s)
+{
+	int count = 0;
+	for (; *s != '\0'; s++)
+	{
+		count++;
+	}
+
+	return (count);
+}
+
+/**
  * _strcpy - Copy a string.
  * @dest: Destination value.
  * @src: Source value.
