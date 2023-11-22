550d9b920ec27f6145210d60456447b48f573274
GIT-justin12 <agbozojustin42@gmail.com>
2023-11-21 20:52:08 -0800

    Update of task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -7,39 +7,45 @@
  * @head: pointer to the first node in the list
  * @idx: index where the new node is added
  * @n: data insert in the new node
+ *
  * Return: pointer to the new node, or NULL
  */
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int i;
-	listint_t *new;
-	listint_t *temp = *head;
+	listint_t *new_node, *iter;
+	unsigned int count = 1;
 
-	new = malloc(sizeof(listint_t));
-
-	if (!new || !head)
-		return (NULL);
-	new->n = n;
-	new->next = NULL;
-
-	if (idx == 0)
+	if (head)
 	{
-		new->next = *head;
-		*head = new;
-		return (new);
-	}
+		new_node = malloc(sizeof(listint_t));
+		if (new_node == NULL)
+			return (NULL);
 
-	for (i = 0; temp && i < idx; i++)
-	{
-		if (i == idx - 1)
+		new_node->n = n;
+		if (idx > 0)
 		{
-			new->next = temp->next;
-			temp->next = new;
-			return (new);
+			iter = *head;
+			while (iter)
+			{
+				if (count == idx)
+				{
+					new_node->next = iter->next;
+					iter->next = new_node;
+					return (new_node);
+				}
+				iter = iter->next;
+				count++;
+			}
+			if (idx > count)
+				return (NULL);
 		}
 		else
-			temp = temp->next;
+		{
+			new_node->next = *head;
+			*head = new_node;
+		}
+		return (new_node);
 	}
 	return (NULL);
 }
