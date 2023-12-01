6f60eac049125e74e99b37a6c2687501d039acc7
GIT-justin12 <agbozojustin42@gmail.com>
2023-12-01 07:15:00 -0800

    Update of task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,28 +12,23 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t nrd, nwr;
-	char *buf;
+	int fd, readed;
+	char *buff = malloc(sizeof(char *) * letters);
 
-	if  (!filename)
+	if (!buff)
 		return (0);
-	fd = open(filename, O_RDONLY);
 
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY, 0600);
 	if (fd == -1)
 		return (0);
 
-	buf = malloc(sizeof(char) * (letters));
+	readed = read(fd, buff, letters);
+	write(STDOUT_FILENO, buff, readed);
 
-	if (!buf)
-		return (0);
-
-	nrd = read(fd, buf, letters);
-	nwr = write(STDOUT_FILENO, buf, nrd);
-
+	free(buff);
 	close(fd);
-
-	free(buf);
-
-	return (nwr);
+	return (readed);
 }
